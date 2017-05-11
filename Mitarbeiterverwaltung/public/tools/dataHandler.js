37f2aae5dbe85868ad9f3d2049b8abc96cfc28ba
Bl3G3 <basti@blackgear-it.de>
2017-05-11 10:01:46 +0200

    Delete dataHandler.js

--- Mitarbeiterverwaltung/public/tools/dataHandler.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by blackgear on 09.05.17.
- */
-function getUserData() {
-    //dummy function, till session code is built
-    var user = {mitarbeiternummer:163, vorname:"Friedrich", nachname: "Friedlein"};
-    return user;
-}
-module.exports.getUserData = getUserData;
